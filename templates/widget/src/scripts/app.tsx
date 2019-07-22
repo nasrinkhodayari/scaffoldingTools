62bbbef53cf41e7fef17915b68d062064e1537d0
nasrinkhodyari <nasrin.khodayari@gmail.com>
2019-07-22 11:56:20 +0430

    scaffoldingTools\templates\widget for first time

---
+++ templates/widget/src/scripts/app.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default class App extends Component {
+  storageKey: string;
+
+  constructor(props: any) {
+    super(props);
+  }
+
+  render(): any {
+    return (
+      <Typography component="div">
+      </Typography>
+    )
+  }
+}
